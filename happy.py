8d3407d3677fed4d8e8e70b25829825c7c06f2fc
maansisangwan <707672@pdsb.net>
2024-06-10 10:19:43 -0700

    Put buttons on top for pages

--- happy.py
+++ happy.py
@@ -10,23 +10,28 @@ from flask import (
 
 
 class User:
-    def __init__(self, id, username, password):
+    def __init__(self, id, username, password,cash):
         self.id = id
         self.username = username
         self.password = password
-
+        self.cash = cash
     def __repr__(self):
         return f'<User: {self.username}>'
 
 
 users = []
-users.append(User(id=1, username='Anthony', password='password'))
-users.append(User(id=2, username='Becca', password='secret'))
-users.append(User(id=3, username='Carlos', password='somethingsimple'))
+users.append(User(id=1, username='Anthony', password='password',cash=10000))
+users.append(User(id=2, username='Becca', password='secret',cash=10000))
+users.append(User(id=3, username='Carlos', password='somethingsimple',cash=10000))
 
 app = Flask(__name__,template_folder='template')
 app.secret_key = 'somesecretkeythatonlyishouldknow'
 
+stockslist = (
+    {'symbol': 'NVDA', 'fullname': 'Nvidia Corporation', 'price' :'1150.37', 'currency' : 'USD', 'country' : 'US'},
+    {'symbol': 'AMD', 'fullname': 'Advanced Micro Devices Inc.', 'price': '160.43', 'currency': 'USD','country': 'US'},
+    {'symbol': 'AAPL', 'fullname': 'Apple Inc.', 'price': '196.12', 'currency': 'USD', 'country': 'US'}
+)
 
 @app.before_request
 def before_request():
@@ -71,13 +76,29 @@ def signup():
 
 
         user_id = len(users) + 1
-        n_u = User(id=user_id, username=username, password=password)
-        users.append(n_u)
+        new_user = User(id=user_id, username=username, password=password, cash=10000)
+        users.append(new_user)
 
-# Redirect to login page
+            # Redirect to login page
         return redirect(url_for('login'))
 
     return render_template('signup.html')
+
+@app.route('/stocks', methods=['GET',"POST"])
+def stocks(templist = stockslist):
+    kwd = ""
+    if request.method=="POST":
+        kwd = request.form['searchkwd']
+        templist = []
+        for stockdict in stockslist:
+            namelist = stockdict.values()
+            for nameid in namelist:
+                if kwd in nameid and stockdict not in templist:
+                    templist.append(stockdict)
+
+        return render_template('stocks.html',stockslist=templist)
+
+    return render_template('stocks.html',stockslist=templist)
 
 @app.route('/home')
 def home():
@@ -85,9 +106,16 @@ def home():
         return redirect(url_for('login'))
     return render_template('home.html')
 
-
 @app.route('/')
-def stock():
+def landing():
+    return redirect('/home')
+
+@app.route('/home2')
+def home2():
     if not g.user:
         return redirect(url_for('login'))
-    return render_template('.html')
+    return render_template('home2.html')
+
+
+if __name__=='__main__':
+    app.run()
